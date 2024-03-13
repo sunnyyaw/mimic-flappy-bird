bac6482c029919093a3b6c89b98b4a8380c70bdc
sunnyyaw <2302886135@qq.com>
2024-03-14 00:28:17 +0800

    add main feat

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const height = canvas.height;
 const mask = document.getElementById('mask');
 const maskText = document.getElementById('mask-span');
 const birdImage = new Image();
+const bottomPillarImage = new Image();
+const topPillarImage = new Image();
+const spriteImage = new Image();
 const ctx = canvas.getContext('2d');
 
 const moveSpeed = 1;
 const pillarNum = 3;
 const holeHeight = height * 0.25;
-const birdSize = { width: width * 0.05, height: height * 0.05 };
+const birdSize = { width: width * 0.06, height: height * 0.04 };
 let paused;
 let birdPosition;
 let birdVelocity;
@@ -18,28 +21,46 @@ let pillars;
 let score;
 let nextPillarIndex;
 let requestId;
+let birdComplete;
+let bottomPillarComplete;
+let topPillarComplete;
+let spriteComplete;
+
+
+birdImage.src = './assets/bird.png';
+bottomPillarImage.src = './assets/bottomPillar.png';
+topPillarImage.src = './assets/topPillar.png';
+spriteImage.src = './assets/sprite.png';
+birdImage.onload = () => birdComplete = true;
+bottomPillarImage.onload = () => bottomPillarComplete = true;
+topPillarImage.onload = () => topPillarComplete = true;
+spriteImage.onload = () => {
+  init();
+  draw();
+  drawMainMenu();
+};
+
+
+window.addEventListener('keydown', event => {
+  if (event.code === 'Space' && paused) {
+    window.cancelAnimationFrame(requestId);
+    mask.style.visibility = 'hidden';
+    start();
+  } else if (event.code === 'Space' ) {
+    birdVelocity.y = -5;
+  } else if(event.code === 'Escape' && !paused) {
+    pause();
+  } 
+});
+window.addEventListener('click', handleClick);
 
 function randomHeight() {
   return (height - holeHeight) * Math.random();
 };
-window.addEventListener('keydown', event => {
-  if (event.code !== 'Space') {
-    return;
-  }
-  if (paused) {
-    window.cancelAnimationFrame(requestId);
-    mask.style.visibility = 'hidden';
-    start();
-  } else {
-    pause();
-  }
-});
-window.addEventListener('click', handleClick);
 
 const init = () => {
   paused = true;
   score = 0;
-  birdImage.src = './assets/bird.jpg';
   birdPosition = { x: width * 0.2, y: height * 0.3 };
   birdVelocity = { x: 0, y: 0 };
   birdAcceleration = { x: 0, y: 0.2 };
@@ -130,12 +151,23 @@ const updatePillars = () => {
 const draw = () => {
   ctx.clearRect(0, 0, width, height);
   
-  ctx.drawImage(birdImage,birdPosition.x, birdPosition.y, birdSize.width, birdSize.height);
+  if (birdComplete) {
+    const deg = Math.atan(birdVelocity.y / moveSpeed );
+    ctx.translate(birdPosition.x + birdSize.width / 2,birdPosition.y + birdSize.height / 2);
+    ctx.rotate(deg);
+    ctx.translate(-birdSize.width / 2, - birdSize.height / 2);
+    ctx.drawImage(birdImage,0,0, birdSize.width, birdSize.height);
+    ctx.setTransform();
+  }
 
   ctx.fillStyle = "#00FF00";
   pillars.forEach(pillar => {
-    ctx.fillRect(pillar.x, 0, pillar.width, pillar.topHeight);
-    ctx.fillRect(pillar.x, height - pillar.bottomHeight, pillar.width, pillar.bottomHeight);
+    if (topPillarComplete) {
+      ctx.drawImage(topPillarImage,pillar.x, 0, pillar.width, pillar.topHeight);
+    }
+    if (bottomPillarComplete) {
+      ctx.drawImage(bottomPillarImage,pillar.x, height - pillar.bottomHeight, pillar.width, pillar.bottomHeight);
+    }
   });
 
   ctx.fillStyle = "#FF0000";
@@ -156,5 +188,20 @@ const refresh = () => {
     window.cancelAnimationFrame(requestId);
   }
 };
-init();
-draw();
+const drawMainMenu = () => {
+  const titleWidth = width * 0.4;
+  const titleHeight = titleWidth / 179 * 48;
+  const titleX = width / 2 - titleWidth / 2;
+  const titleY = height * 0.3 - titleHeight / 2;
+  const btnWidth = width * 0.2;
+  const btnHeight = btnWidth / 104 * 58;
+  const btnStartX = width / 3 - btnWidth / 2;
+  const btnRankX = width * 2/ 3 - btnWidth / 2;
+  const btnY = height * 2/ 3 - btnHeight / 2;
+  ctx.drawImage(spriteImage,702,182,179,48,
+    titleX,titleY,titleWidth,titleHeight);
+  ctx.drawImage(spriteImage,708,236,104,58,
+    btnStartX,btnY,btnWidth,btnHeight );
+  ctx.drawImage(spriteImage,828,236,104,58,
+    btnRankX,btnY,btnWidth,btnHeight );
+};
